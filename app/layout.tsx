c7c76ef242b4ac546ddd3b02dcb3f18cd8fe0c61
madsemilgodstil <mads93n2@stud.kea.dk>
2024-09-17 15:07:31 +0200

    next.js test

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,17 @@
+import "@/app/ui/global.css";
+import { poppins } from "./ui/fonts";
+
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode;
+  children: React.ReactNode,
 }) {
   return (
     <html lang="en">
-      <body className={` bg-black antialiased`}>{children}</body>
+      <body className={`${poppins.className} bg-black antialiased`}>
+        {children}
+      </body>
     </html>
   );
 }
+  
